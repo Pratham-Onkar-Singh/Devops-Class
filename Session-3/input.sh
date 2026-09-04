519d9ce33bb23d3c959800f15da505013e9f2603
Pratham Onkar Singh <prathamaj09@gmail.com>
2026-09-04 19:59:09 +0530

    Delete Session-3 directory

--- Session-3/input.sh
+++
@@ -1,7 +0,0 @@
-read -p "ur naam: " name
-read -p "ur roll: " roll_no
-read -p "2 words: " comm
-
-echo "$name"
-echo "$roll_no"
-echo "$comm"
--- Session-3/variable.sh
+++
@@ -1,7 +0,0 @@
-name="pratham"
-roll_no="136"
-comment="Ye le"
-
-echo "My name is $name"
-echo "Me roll $roll_no"
-echo "$comment"
